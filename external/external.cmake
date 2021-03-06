3aabec138f66d8a93861c619e47cdf2171b03e93
Arthur LAURENT <arthur.laurent4@gmail.com>
2021-03-06 11:25:22 +0100

    Using patched cppast for C++20

--- external/external.cmake
+++ external/external.cmake
@@ -8,6 +8,8 @@
 message(STATUS "Installing cppast via submodule")
 execute_process(COMMAND git submodule update --init -- external/cppast
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+execute_process(COMMAND git checkout features/AddCPP20SupportAndConsteval -- external/cppast
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 add_subdirectory(external/cppast EXCLUDE_FROM_ALL)
 
 #
